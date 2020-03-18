7053d37500bbba12d26705e02be93fce81ef2b13
Corniel Nobel <mail@corniel.nl>
2020-03-18 20:49:35 +0100

    Added a test with header too.

--- test/Ivory.Soap.UnitTests/SoapResultTest.cs
+++ test/Ivory.Soap.UnitTests/SoapResultTest.cs
@@ -1,11 +1,6 @@
-﻿using Ivory.Soap.UnitTests.Mocking;
-using Ivory.Soap.UnitTests.Models;
+﻿using Ivory.Soap.UnitTests.Models;
 using Ivory.Soap.UnitTests.TestTools;
-using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Ivory.Soap.UnitTests
@@ -17,8 +12,26 @@ namespace Ivory.Soap.UnitTests
         {
             var result = new SoapResult(header: null, body: new SimpleBody { Value = 17 });
 
+            await ActionResultAssert.WritesToBody(@"<SOAP-ENV:Envelope SOAP-ENV:encodingStyle=""http://www.w3.org/2001/12/soap-encoding"" xmlns:SOAP-ENV=""http://www.w3.org/2001/12/soap-envelope"">
+  <SOAP-ENV:Body>
+    <SimpleBody xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+      <Value>17</Value>
+    </SimpleBody>
+  </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>", result);
+        }
+
+        [Test]
+        public async Task SoapResult_WithHeaderAndBody()
+        {
+            var result = new SoapResult(header: new SimpleHeader { Message = "Hello" }, body: new SimpleBody { Value = 17 });
 
             await ActionResultAssert.WritesToBody(@"<SOAP-ENV:Envelope SOAP-ENV:encodingStyle=""http://www.w3.org/2001/12/soap-encoding"" xmlns:SOAP-ENV=""http://www.w3.org/2001/12/soap-envelope"">
+  <SOAP-ENV:Header>
+    <SimpleHeader xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+      <Message>Hello</Message>
+    </SimpleBody>
+  </SOAP-ENV:Header>
   <SOAP-ENV:Body>
     <SimpleBody xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
       <Value>17</Value>
