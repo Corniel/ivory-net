adc46f0bfd33439883263c3d03bce80772e8b6d5
Corniel Nobel <mail@corniel.nl>
2020-03-20 09:50:30 +0100

    Made a propper XNodeDeserializer extension class.

---
+++ src/Ivory.Soap/Extensions/XNodeDeserialize.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Serialization;
+
+namespace System.Xml.Linq
+{
+    /// <summary>Reperesents an <see cref="XNode"/> deserializer.</summary>
+    public static class XNodeDeserialize
+    {
+        /// <summary>Deserializes the XML node.</summary>
+        /// <param name="node">
+        /// The node to deserialize.
+        /// </param>
+        /// <param name="type">
+        /// The type to deserilize it too.
+        /// </param>
+        /// <returns>
+        /// A deserialized object.
+        /// </returns>
+        public static object Deserialize(this XNode node, Type type)
+        {
+            if (node is null)
+            {
+                return null;
+            }
+            if (node.GetType() == type)
+            {
+                return node;
+            }
+            var reader = node.CreateReader();
+
+            if (typeof(IXmlSerializable).IsAssignableFrom(type))
+            {
+                var xmlSerializable = (IXmlSerializable)Activator.CreateInstance(type);
+                xmlSerializable.ReadXml(reader);
+                return xmlSerializable;
+            }
+            var serializer = new XmlSerializer(type);
+            return serializer.Deserialize(reader);
+        }
+    }
+}
---
+++ test/Ivory.Soap.UnitTests/Extensions/XNodeDeserializeTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Qowaiv;
+using System.Xml.Linq;
+
+namespace Ivory.Soap.UnitTests.Extensions
+{
+    public class XNodeDeserializeTest
+    {
+        [Test]
+        public void Deserialize_Null_IsNull()
+        {
+            XElement xText = null;
+
+            var deserialized = xText.Deserialize(typeof(PostalCode));
+            Assert.IsNull(deserialized);
+        }
+        [Test]
+        public void Deserialize_ToSameType_AreIdentical()
+        {
+            var expected = new XElement("dummy", "2624DP");
+            var deserialized = expected.Deserialize(typeof(XElement));
+
+            Assert.AreSame(expected, deserialized);
+        }
+
+        [Test]
+        public void Deserialize_IXmlSerializable_IsDeserialzed()
+        {
+            var xText = new XElement("dummy", "2624DP");
+
+            var deserialized = xText.Deserialize(typeof(PostalCode));
+            var expected = PostalCode.Parse("2624DP");
+
+            Assert.AreEqual(expected, deserialized);
+        }
+    }
+}
