ea69ca78900106ca8b3c948e0e974018dd607738
Corniel Nobel <mail@corniel.nl>
2020-03-20 13:41:07 +0100

    Adding a README.md

---
+++ README.md
@@ -0,0 +1,49 @@
+# Ivory.NET SOAP
+Ivory.NET SOAP is lightweight library on top of
+[ASP.NET Core 3.1](https://docs.microsoft.com/en-us/aspnet/core).
+
+## SOAP endpoint
+A custom controller could look like:
+``` C#
+[SoapV1_2]
+public class CustomController : ControllerBase
+{
+    [SoapAction("http://custom.org/custom-action")]
+    public IActionResult CustomAction(Header header, Body body)
+    {
+        // Do/call logic.
+        return this.Soap(body: SomeResult());
+    }
+}
+```
+The `SoupAction` attribute is required, as without it, the routing could not
+know, how to reach the specific endpoint. The `this.Soap()` extension method
+ensures the correct output taking the `SoapVersion` attribute (1.2 in this
+example), into account, where specifying the SOAP version is optional.
+
+The Startup.cs should look a bit like this:
+
+``` C#
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers(options =>
+        {
+            options.ModelBinderProviders.Insert(0, new SoapModelBinder());
+        });
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
+```
+In other words, the only thing that really has to be done (besides enabling
+ASP.NET Core itself) is the registration of the `SoapModelBinder`.
--- test/Ivory.SoapApi/Startup.cs
+++ test/Ivory.SoapApi/Startup.cs
@@ -7,43 +7,34 @@ using Microsoft.Extensions.Hosting;
 
 namespace Ivory.SoapApi
 {
-    public class Startup
+public class Startup
+{
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers(options =>
         {
-            Configuration = configuration;
-        }
+            options.ModelBinderProviders.Insert(0, new SoapModelBinder());
+        });
 
-        public IConfiguration Configuration { get; }
+    }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
         {
-            services.AddControllers(options =>
-            {
-                options.ModelBinderProviders.Insert(0, new SoapModelBinder());
-            });
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+            endpoints.MapControllers();
+        });
+    }
     }
 }
