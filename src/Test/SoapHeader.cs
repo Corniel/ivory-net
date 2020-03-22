b56d0718dabb0e3f8592867a179462725edbaf5d
Corniel Nobel <mail@corniel.nl>
2020-03-22 19:34:20 +0100

    clean-up experimenting code.

--- src/Test/SoapHeader.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using System.Xml.Serialization;
-
-namespace XXX
-{
-    [XmlRoot(Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
-    public class SoapHeader<THeader> : List<THeader> { }
-}
