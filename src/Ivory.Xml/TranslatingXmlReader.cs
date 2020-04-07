5fed5bd84bd03d59bd63a61cabc131cad4d69920
Corniel Nobel <mail@corniel.nl>
2020-04-07 10:55:50 +0200

    in Ivory.Xml.

--- src/Ivory.Xml/TranslatingXmlReader.cs
+++ src/Ivory.Xml/TranslatingXmlReader.cs
@@ -1,40 +1,26 @@
-﻿using System.Collections.Generic;
-using System.Xml;
+﻿using System.Xml;
 using System.Xml.Linq;
 
-namespace Ivory.Soap.Xml
+namespace Ivory.Xml
 {
     public class TranslatingXmlReader : XmlReader
     {
         private readonly XmlReader reader;
-        private readonly Dictionary<XName, XName> xNameTranslations = new Dictionary<XName, XName>();
-        private readonly Dictionary<string, string> localNameTranslations = new Dictionary<string, string>();
-        private readonly Dictionary<string, string> namespaceUriTranslations = new Dictionary<string, string>();
+        private readonly IXNameTransform transform;
 
         /// <summary>Initializes a new instance of the <see cref="TranslatingXmlReader"/> class.</summary>
         /// <param name="reader">
         /// The underlying XML reader.
         /// </param>
-        public TranslatingXmlReader(XmlReader reader) => this.reader = Guard.NotNull(reader, nameof(reader));
+        public TranslatingXmlReader(XmlReader reader) => this.reader = reader;
 
         /// <summary>Gets the (potential translated) local name.</summary>
         public override string LocalName
         {
             get
             {
-                var original = reader.NamespaceURI;
-
-                if (xNameTranslations.TryGetValue(XName.Get(reader.LocalName, original), out var transXname))
-                {
-                    return transXname.LocalName;
-                }
-
-                if (localNameTranslations.TryGetValue(original, out var translated))
-                {
-                    return translated;
-                }
-
-                return original;
+                var original = XName.Get(reader.LocalName, reader.NamespaceURI);
+                return transform.Tranform(original)?.LocalName ?? original.LocalName;
             }
         }
 
@@ -43,50 +29,9 @@ namespace Ivory.Soap.Xml
         {
             get
             {
-                var original = reader.NamespaceURI;
-
-                if (xNameTranslations.TryGetValue(XName.Get(reader.LocalName, original), out var transXname))
-                {
-                    return transXname.NamespaceName;
-                }
-
-                if (namespaceUriTranslations.TryGetValue(original, out var translated))
-                {
-                    return translated;
-                }
-
-                return original;
+                var original = XName.Get(reader.LocalName, reader.NamespaceURI);
+                return transform.Tranform(original)?.NamespaceName ?? original.NamespaceName;
             }
-        }
-
-        /// <summary>Adds a local name translation.</summary>
-        public TranslatingXmlReader AddTranslation(string orignal, string translation)
-        {
-            Guard.NotNull(orignal, nameof(orignal));
-            Guard.NotNull(translation, nameof(translation));
-
-            localNameTranslations.Add(orignal, translation);
-            return this;
-        }
-
-        /// <summary>Adds a full (local name and namespace) translation.</summary>
-        public TranslatingXmlReader AddTranslation(XName orignal, XName translation)
-        {
-            Guard.NotNull(orignal, nameof(orignal));
-            Guard.NotNull(translation, nameof(translation));
-
-            xNameTranslations.Add(orignal, translation);
-            return this;
-        }
-
-        /// <summary>Adds a namespace translation.</summary>
-        public TranslatingXmlReader AddTranslation(XNamespace orignal, XNamespace translation)
-        {
-            Guard.NotNull(orignal, nameof(orignal));
-            Guard.NotNull(translation, nameof(translation));
-
-            namespaceUriTranslations.Add(orignal.NamespaceName, translation.NamespaceName);
-            return this;
         }
 
         #region Via this.reader
