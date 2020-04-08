a5f19baeec2e92460ecef46687ab2de4ac439c7b
Corniel Nobel <mail@corniel.nl>
2020-04-08 13:59:51 +0200

    Using XML overrides.

--- test/Ivory.UnitTests/Xml/ExtendedXmlReaderTest.cs
+++ test/Ivory.UnitTests/Xml/ExtendedXmlReaderTest.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using System;
 using System.Xml;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace Ivory.UnitTests.Xml
 {
@@ -24,5 +25,42 @@ namespace Ivory.UnitTests.Xml
 
             Assert.AreEqual(expected, xmlString);
         }
+
+        [Test]
+        public void Deserialize_StrictOrder()
+        {
+            using var reader = Message.EmbeddedReader("StrictOrder.xml");
+
+            var overrides = new XmlAttributeOverrides();
+            
+            foreach(var prop in typeof(StrictOrder).GetProperties())
+            {
+                var attributes = new XmlAttributes();
+                var attr = new XmlElementAttribute(prop.Name, prop.PropertyType);
+                attributes.XmlElements.Add(attr);
+                overrides.Add(prop.DeclaringType, prop.Name, attributes);
+            }
+            
+        
+            var serializer = new XmlSerializer(typeof(StrictOrder), overrides);
+
+            var model = (StrictOrder)serializer.Deserialize(reader);
+
+            Assert.AreEqual("What's in it?", model.Name);
+            Assert.AreEqual(666, model.Value);
+            Assert.AreEqual("extra", model.Extra);
+        }
+    }
+
+    public class StrictOrder
+    {
+        [XmlElement(Order = 1)]
+        public int Value { get; set; }
+        
+        [XmlElement(Order = 2)]
+        public string Name { get; set; }
+
+        [XmlElement(Order = 3)]
+        public string Extra { get; set; }
     }
 }
